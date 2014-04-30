672b3aa49292c9539ae9e6e2f2d4baaa7caedeab
MrWolfZ <ziller.jonathan@web.de>
2014-04-30 20:23:48 +0200

    initial project commit

---
+++ Packaging/Lua/LuaCompiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Packaging.Lua
+{
+    public class LuaCompiler
+    {
+        private readonly string compilerPath;
+
+        public LuaCompiler(string compilerPath)
+        {
+            this.compilerPath = compilerPath;
+        }
+
+        public void Compile(string sourceDirectoryPath, string outputDirectoryPath = null)
+        {
+            var sourceDir = new DirectoryInfo(sourceDirectoryPath);
+
+            if (!sourceDir.Exists)
+            {
+                return;
+            }
+
+            var outputDir = outputDirectoryPath != null ? new DirectoryInfo(outputDirectoryPath) : sourceDir.Parent;
+            var files = sourceDir.EnumerateFiles("*.lua");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = this.compilerPath;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            foreach (var file in files)
+            {
+                var source = string.Format(@"{0}\{1}", sourceDir.FullName, file.Name);
+                var output = string.Format(@"{0}\{1}", outputDir.FullName, file.Name.Replace(file.Extension, ".xpl"));
+                startInfo.Arguments = string.Format("-bg \"{0}\" \"{1}\"", source, output);
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    if (exeProcess.ExitCode != 0)
+                    {
+                        throw new Exception(string.Format("Compiler exited with exit code {0}", exeProcess.ExitCode));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UFO/Extension/ui/addons/detailmonitor/sources/ufo_menu_configuration.lua
@@ -0,0 +1,100 @@
+-- persistent state
+local Functions, Buttons = {}, {}
+
+-- register the new UFO menu
+local function init()
+	if LibMJ and LibMJ.RegisterMenu then
+		LibMJ:RegisterMenu("UFOConfiguration", 
+							nil, -- onMenuInit, 
+							nil, -- onMenuClosed, 
+							nil, -- onReturnArgsReceived 
+							Functions.titleProvider, 
+							Functions.rowProvider,
+							{ type = "buttons", provider = Functions.buttonProvider })
+	else
+		LibMJ = LibMJ or { registerFuncs = {} }
+		table.insert(LibMJ.registerFuncs, init)
+	end
+end
+
+Functions.titleProvider = function (state)
+	return ReadText(99998,9000) -- "UFO Configuration"
+end
+
+Functions.rowProvider = function (state, rowCollection)
+	local buttonFontSize = 12
+
+	local emptyRow = LibMJ:Row({ LibMJ:Cell(nil, nil, 10) }, nil, LibMJ.colors.transparent, true)
+	
+	table.insert(rowCollection, emptyRow)
+	
+	local cells = {}
+	table.insert(cells, LibMJ:Cell(ReadText(99997,9001) .. ":")) -- "Debug Level:"
+	table.insert(cells, LibMJ:Cell(UFO.debugLevel.selected.label))
+	
+	local setErrorButtonLabel = UFO.debugLevel.error.label
+	local setErrorButtonScript = function() 
+		UFO.debugLevel.selected = UFO.debugLevel.error
+		LibMJ:RefreshMenu()
+	end	
+	table.insert(cells, LibMJ:ButtonCell(setErrorButtonLabel, setErrorButtonScript, 1, true, nil, buttonFontSize))
+	
+	table.insert(cells, LibMJ:Cell())
+	
+	local setInfoButtonLabel = UFO.debugLevel.info.label
+	local setInfoButtonScript = function() 
+		UFO.debugLevel.selected = UFO.debugLevel.info
+		LibMJ:RefreshMenu()
+	end	
+	table.insert(cells, LibMJ:ButtonCell(setInfoButtonLabel, setInfoButtonScript, 1, true, nil, buttonFontSize))
+	
+	table.insert(cells, LibMJ:Cell())
+	
+	local setFineButtonLabel = UFO.debugLevel.fine.label
+	local setFineButtonScript = function() 
+		UFO.debugLevel.selected = UFO.debugLevel.fine
+		LibMJ:RefreshMenu()
+	end	
+	table.insert(cells, LibMJ:ButtonCell(setFineButtonLabel, setFineButtonScript, 1, true, nil, buttonFontSize))
+	
+	table.insert(cells, LibMJ:Cell())	
+	
+	local setFinestButtonLabel = UFO.debugLevel.finest.label
+	local setFinestButtonScript = function() 
+		UFO.debugLevel.selected = UFO.debugLevel.finest
+		LibMJ:RefreshMenu()
+	end	
+	table.insert(cells, LibMJ:ButtonCell(setFinestButtonLabel, setFinestButtonScript, 1, true, nil, buttonFontSize))
+	
+	table.insert(cells, LibMJ:Cell())	
+	
+	table.insert(rowCollection, LibMJ:Row(cells, nil, LibMJ.colors.transparent, true))
+	
+	local colWidths = { 180, 100, 100, 20, 100, 20, 100, 20, 100, 0 }
+	return colWidths
+end
+
+Functions.buttonProvider = function (state)
+	local buttons = {}
+	
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	table.insert(buttons, LibMJ:BarButton())
+	
+	local uninstallButtonLabel = ReadText(99997,9000) -- "Uninstall"
+	local uninstallButtonSript = Buttons.uninstall
+	table.insert(buttons, LibMJ:BarButton(uninstallButtonLabel, uninstallButtonSript))
+	
+	return buttons
+end
+
+Buttons.uninstall = function ()
+	UFO.uninstall = true
+	LibMJ:CloseMenu()
+end
+
+init()
